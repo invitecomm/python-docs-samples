1d8669ddfef198ae6ab163f574c7e2498aa3080d
Brian LaVallee <brian.lavallee@invite-comm.jp>
2016-09-06 14:15:55 +0900

    Changed to support Asterisk ulaw.

--- speech/api/speech_rest.py
+++ speech/api/speech_rest.py
@@ -62,8 +62,8 @@ def main(speech_file):
             'config': {
                 # There are a bunch of config options you can specify. See
                 # https://goo.gl/EPjAup for the full list.
-                'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
-                'sampleRate': 16000,  # 16 khz
+                'encoding': 'MULAW',  # raw 16-bit signed LE samples
+                'sampleRate': 8000,  # 16 khz
                 # See https://goo.gl/DPeVFW for a list of supported languages.
                 'languageCode': 'en-US',  # a BCP-47 language tag
             },
